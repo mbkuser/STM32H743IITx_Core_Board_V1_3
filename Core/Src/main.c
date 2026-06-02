3ac7a282557687cc68ae6eef6f6e86f7b769210f
mbk <mbkmailbox@gmail.com>
2026-06-02 11:03:20 +0300

    Second commit. Add text in main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,7 +23,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "sdram.h"
-
+//добавил git
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
